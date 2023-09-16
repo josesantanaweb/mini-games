c5bce038f1f749f4cff44a39c8c9c723d670647d
josesantana.web <josesantana.web@gmail.com>
2023-09-16 13:37:57 -0400

    fix: change seo text

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,8 @@ import { Nunito_Sans } from "next/font/google"
 const nunito = Nunito_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Chicken Game",
-  description: "Chicken Game",
+  title: "Mini Games",
+  description: "Mini Games",
 };
 
 export default function RootLayout({
